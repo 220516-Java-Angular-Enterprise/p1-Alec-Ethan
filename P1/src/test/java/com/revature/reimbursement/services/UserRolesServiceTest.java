2b47678e07731f5f7d44f0b0ccdd97cfa9813a33
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-06-04 12:39:25 -0400

    Added demo test case

---
+++ P1/src/test/java/com/revature/reimbursement/services/UserRolesServiceTest.java
@@ -0,0 +1,17 @@
+package com.revature.reimbursement.services;
+
+import com.revature.reimbursement.models.UserRoles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRolesServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void getRole_id() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRole() {
+    }
+}
