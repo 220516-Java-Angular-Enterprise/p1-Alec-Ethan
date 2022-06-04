dc40b691f27a08c7635aab226eccaf64983565c8
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-06-04 12:18:11 -0400

    Initial Commit

---
+++ P1/src/main/java/com/revature/reimbursement/daos/CrudDAO.java
@@ -0,0 +1,10 @@
+package com.revature.reimbursement.daos;
+import java.util.List;
+
+public interface CrudDAO<T> {
+    void save(T obj);
+    void update(T obj);
+    void delete(String id);
+    T getById(String id);
+    List<T> getAll();
+}
---
+++ P1/src/main/java/com/revature/reimbursement/testui/AlecUI.java
@@ -0,0 +1,4 @@
+package com.revature.reimbursement.testui;
+
+public class AlecUI {
+}
