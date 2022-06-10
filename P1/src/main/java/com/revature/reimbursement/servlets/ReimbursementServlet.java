b95d3075731b54823f24967ad83c04b9dba32e58
despres99 <ethan.despres.99@gmail.com>
2022-06-10 09:40:08 -0400

    Created the Display Reimbursement Functions
    Not yet tested

---
+++ P1/src/main/java/com/revature/reimbursement/servlets/ReimbursementServlet.java
@@ -0,0 +1,92 @@
+package com.revature.reimbursement.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.reimbursement.dtos.responses.Principal;
+import com.revature.reimbursement.models.Reimbursements;
+import com.revature.reimbursement.services.ReimbursementsService;
+import com.revature.reimbursement.services.TokenService;
+import com.revature.reimbursement.services.UserRolesService;
+import com.revature.reimbursement.services.UsersService;
+import com.revature.reimbursement.util.annotations.Inject;
+import com.revature.reimbursement.util.customException.InvalidRequestException;
+import com.revature.reimbursement.util.customException.ResourceConflictException;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ReimbursementServlet extends HttpServlet {
+
+    @Inject
+    private final ObjectMapper mapper;
+    private final UsersService usersService;
+    private final UserRolesService userRolesService;
+    private final TokenService tokenService;
+    private final ReimbursementsService reimbursementsService;
+
+    @Inject
+    public ReimbursementServlet(ObjectMapper mapper, UsersService usersService, UserRolesService userRolesService, TokenService tokenService, ReimbursementsService reimbursementsService) {
+        this.mapper = mapper;
+        this.usersService = usersService;
+        this.userRolesService = userRolesService;
+        this.tokenService = tokenService;
+        this.reimbursementsService = reimbursementsService;
+    }
+
+    //I want to create a doGet and this will show different things depending on the user role
+    //EMPLOYEE: Display all reimbursements THEY CREATED
+    //FINANCE MANAGER: Display all reimbursements
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Principal requester = tokenService.extractRequesterDetails(req.getHeader("Authorization"));
+            if (requester == null) {
+                resp.setStatus(401); // UNAUTHORIZED
+                return;
+            }
+            String role = userRolesService.getById(requester.getRole()).getRole();
+            if (role.equals("FINANCE MANAGER")) {
+                //Display all Reimbursements
+                List<Reimbursements> rems = reimbursementsService.getAll();
+                resp.setContentType("application/json");
+                resp.getWriter().write(mapper.writeValueAsString(rems));
+            }
+            else if (role.equals("EMPLOYEE")) {
+                //Display all Reimbursements by Author ID
+                List<Reimbursements> rems = reimbursementsService.getAllRowsByColumnValue("author_id", requester.getId());
+                resp.setContentType("application/json");
+                resp.getWriter().write(mapper.writeValueAsString(rems));
+            }
+
+        } catch (InvalidRequestException e) {
+            resp.setStatus(404); // BAD REQUEST
+            e.printStackTrace();
+        } catch (ResourceConflictException e) {
+            resp.setStatus(409); // RESOURCE CONFLICT
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+
+    //EMPLOYEE: Create a new Reimbursement to the Database
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+
+        } catch (InvalidRequestException e) {
+            resp.setStatus(404); // BAD REQUEST
+            e.printStackTrace();
+        } catch (ResourceConflictException e) {
+            resp.setStatus(409); // RESOURCE CONFLICT
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+}
--- P1/src/main/java/com/revature/reimbursement/util/ContextLoaderListener.java
+++ P1/src/main/java/com/revature/reimbursement/util/ContextLoaderListener.java
@@ -1,12 +1,15 @@
 package com.revature.reimbursement.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.reimbursement.daos.ReimbursementsDAO;
 import com.revature.reimbursement.daos.UserRolesDAO;
 import com.revature.reimbursement.daos.UsersDAO;
+import com.revature.reimbursement.services.ReimbursementsService;
 import com.revature.reimbursement.services.TokenService;
 import com.revature.reimbursement.services.UserRolesService;
 import com.revature.reimbursement.services.UsersService;
 import com.revature.reimbursement.servlets.LoginServlet;
+import com.revature.reimbursement.servlets.ReimbursementServlet;
 import com.revature.reimbursement.servlets.UsersServlet;
 
 import jakarta.servlet.ServletContext;
@@ -25,11 +28,13 @@ public class ContextLoaderListener implements ServletContextListener {
         /* Dependency injection. */
         UsersServlet userServlet = new UsersServlet(mapper, new UsersService(new UsersDAO()), new UserRolesService(new UserRolesDAO()), new TokenService(new JwtConfig()));
         LoginServlet loginServlet = new LoginServlet(mapper, new UsersService(new UsersDAO()), new TokenService(new JwtConfig()));
+        ReimbursementServlet reimbursementServlet = new ReimbursementServlet(mapper, new UsersService(new UsersDAO()), new UserRolesService(new UserRolesDAO()), new TokenService(new JwtConfig()), new ReimbursementsService(new ReimbursementsDAO()));
 
         /* Need ServletContext class to map whatever servlet to url path. */
         ServletContext context = sce.getServletContext();
         context.addServlet("UsersServlet", userServlet).addMapping("/users/*");
         context.addServlet("LoginServlet", loginServlet).addMapping("/login/*");
+        context.addServlet("ReimbursementServlet", reimbursementServlet).addMapping("/reimbursements/*");
     }
 
     @Override
