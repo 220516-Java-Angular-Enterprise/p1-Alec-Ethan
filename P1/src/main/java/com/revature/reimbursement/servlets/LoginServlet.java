0b19e0e55fb457dc338cd6dd21d82713b44f4f14
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-06-13 11:41:12 -0400

    Fixed User Registration and Admin approval

--- P1/src/main/java/com/revature/reimbursement/servlets/LoginServlet.java
+++ P1/src/main/java/com/revature/reimbursement/servlets/LoginServlet.java
@@ -10,6 +10,7 @@ import com.revature.reimbursement.services.UsersService;
 import com.revature.reimbursement.util.annotations.Inject;
 import com.revature.reimbursement.util.customException.InvalidRequestException;
 import com.revature.reimbursement.util.customException.ResourceConflictException;
+import com.revature.reimbursement.util.customException.UnauthorizedException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -36,10 +37,18 @@ public class LoginServlet extends HttpServlet {
             NewLoginRequest request = mapper.readValue(req.getInputStream(), NewLoginRequest.class);
             Principal principal = new Principal(userService.login(request));
 
+            boolean activeUser = userService.getRowByColumnValue("id", "'" + principal.getId() + "'").isIs_active();
+            if (!activeUser){
+                throw new UnauthorizedException();
+            }
+
+
             String token = tokenService.generateToken(principal);
             resp.setHeader("Authorization", token);
             resp.setContentType("application/json");
             resp.getWriter().write(mapper.writeValueAsString(principal));
+        } catch (UnauthorizedException e){
+            resp.setStatus(401);
         } catch (InvalidRequestException e) {
             resp.setStatus(404); // BAD REQUEST
             e.printStackTrace();
